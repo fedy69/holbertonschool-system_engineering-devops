119487feccde3238b01da0aac0ee83e6a667a93f
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-06-04 01:01:30 +0100

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # fixes Apache error by fixing typo in wordpress
 
 exec { 'fix-wordpress':
-   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-   path    => '/usr/local/bin/:/bin/'
-   
-   }
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
