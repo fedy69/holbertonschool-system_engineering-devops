3e909010dc346afd154432d4910edeffa0b4acf5
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-06-09 23:31:43 +0100

    nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,11 @@
 #the amount of traffic an Nginx server can handle
 exec {'replace':
   provider => shell,
-  command  => ' sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\',
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   before   => Exec['restart'],
 }
+
+
 #Restart Nginx
 
 exec {'restart':
