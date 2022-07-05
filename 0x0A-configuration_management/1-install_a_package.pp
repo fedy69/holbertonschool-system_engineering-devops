e2ccce52849d3766c77e427ac6bc428b4b96de82
Fedy chihaoui <sasouciouchiha@gmail.com>
2022-07-05 06:29:34 +0100

    install puppet lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Puppet manifest to install 'puppet-lint'
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Using Puppet, install puppet-lint
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
